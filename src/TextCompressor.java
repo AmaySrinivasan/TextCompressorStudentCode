d2ed4406a4a912dff2a0227ffd2ec5838dd0d0d7
amaysrinivasan <amay.srinivasan@menloschool.org>
2025-12-14 22:26:57 -0800

    Final

--- src/TextCompressor.java
+++ src/TextCompressor.java
@@ -22,120 +22,97 @@
  ******************************************************************************/
 
 /**
- *  The {@code TextCompressor} class provides static methods for compressing
- *  and expanding natural language through textfile input.
+ * The {@code TextCompressor} class provides static methods for compressing
+ * and expanding natural language through textfile input.
  *
- *  @author Zach Blick, Amay Srinivasan
+ * @author Zach Blick, Amay Srinivasan
  */
 public class TextCompressor {
-    // Here;s what I'm thinking for the basic idea,  befroe Mr. Blick shows the answer
-    // Because it is really redundant and words like the, and, said, Alice are repeated
-    // And punctuation is also repeated, and characters are not equally common, like space
-    // and e, a, o, are also common, they don't really need 8-bit codes.
-    // I need to basically create a dictionary type of text compressor, where I think I build a
-    // frequency table for chars, where i scan through it once at count occurence for each char
-    // and then build code for the characters to match them
-    // And then I also need to build a dictionary of the most common words
-    // And create an output for each word in the dictionary, otherwise output raw characters
-    private static final int TABLE_SIZE = 257;
-    private static char[] keys = new char[TABLE_SIZE];
-    private static int[] vals = new int[TABLE_SIZE];
-    private static boolean[] used = new boolean[TABLE_SIZE];
+    // Number of ASCII characters
+    private static final int R = 128;
+    // End of File marker at hex 0x80
+    private static final int EOF = 128;
+    // Total number of available codeword for 8-bit size
+    private static final int L = 256;
+    // Width of each codeword in number of bits
+    private static final int W = 8;
 
-    private static int[] bitLength = new int[TABLE_SIZE];
-    private static String[] code = new String[TABLE_SIZE];
-    private static int hash(char c) {
-        int h = c;
-        h = (h*31 + 7) & TABLE_SIZE;
-        if (h<0) {
-            h += TABLE_SIZE;
-        }
-        return h;
-    }
-    private static int findSlot (char c) {
-        int i = hash(c);
-        while (used[i] && keys[i] != c) {
-            i = (i+1) % TABLE_SIZE;
-        }
-        return i;
-    }
+    // Compress the input using LZW and writes encoded bitsream to ouput
     private static void compress() {
-
-        // TODO: Complete the compress() method
-        String s = BinaryStdIn.readString();
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            int pos = findSlot(c);
-            if (!used[pos]) {
-                used[pos] = true;
-                keys[pos] = c;
-                vals[pos] = 1;
-            } else {
-                vals[pos]++;
-            }
+        // Reads the entire inputted text into memory
+        String input = BinaryStdIn.readString();
+        // Using a TST dictionary mapping strings to codewords, initializing with all single character strings in ASCII
+        TST tst = new TST();
+        for (int i = 0; i < R; i++) {
+            tst.insert("" + (char) i, i);
         }
-        int count = 0;
-        for (int i = 0; i < TABLE_SIZE; i++) {
-            if (used[i]) {
-                count++;
+        // Next available dictionary code (starting after EOF) (129)
+        int nextCode = R + 1;
+        int index = 0;
+        while (index < input.length()) {
+            // Finds longest prefix of input starting at index existing in the dictionary, then writes matching codeword
+            String prefix = tst.getLongestPrefix(input, index);
+            int code = tst.lookup(prefix);
+            BinaryStdOut.write(code, W);
+            // If possible, adds new dictionary entry with prefix + next character in the input
+            if (index + prefix.length() < input.length() && nextCode < L) {
+                String newEntry = prefix + input.charAt(index + prefix.length());
+                tst.insert(newEntry, nextCode++);
             }
+            // Moves forward by the length of the matched prefix
+            index += prefix.length();
         }
-        char[] chars = new char[count];
-        int[] frew = new int[count];
-
-        int idx = 0;
-        for (int i = 0; i < TABLE_SIZE; i++) {
-            if (used[i]) {
-                chars[idx] = keys[i];
-                frew[idx] = vals[i];
-                idx++;
-            }
-        }
-        for (int i = 0; i < count; i++) {
-            bitLength[i] = 4 + (i/10);
-            if (bitLength[i] > 12) {
-                bitLength[i] = 12;
-            }
-            String b = Integer.toBinaryString(i);
-            while (b.length() < bitLength[i]) {
-                b = "0" + b;
-            }
-            code[i] = b;
-        }
-        BinaryStdOut.write((char) count);
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            int pos = findSlot(c);
-            int index = -1;
-            for (int j = 0; j < count; j++) {
-                if (chars[j] == c) {
-                    index = j;
-                    break;
-                }
-            }
-            String bits = code[index];
-            for (int k = 0; k < bits.length(); k++) {
-                if (bits.charAt(k) == '1') {
-                    BinaryStdOut.write(true);
-                } else {
-                    BinaryStdOut.write(false);
-                }
-            }
-
-        }
+        // WRites the End of File code so that it knows when to stop
+        BinaryStdOut.write(EOF, W);
         BinaryStdOut.close();
+
     }
 
+    // Expands LZW compressed stream that was inputted and writes original text to output
     private static void expand() {
-        // Didn't get to this
-        // TODO: Complete the expand() method
-
+        // Dictionary maps codewords to strings, intializing with single-character ASCII strings
+        String[] table = new String[L];
+        for (int i = 0; i < R; i++) {
+            table[i] = "" + (char) i;
+        }
+        // Next available code at 129 (after the ASCIIs)
+        int nextCode = R + 1;
+        // Reads first codeword and if the file is empty, just exist immediatley
+        int codeWord = BinaryStdIn.readInt(W);
+        if (codeWord == EOF) {
+            return;
+        }
+        // The current decoded string
+        String val = table[codeWord];
+        while (true) {
+            // Outputs current decoded string and reads the next codeword
+            BinaryStdOut.write(val);
+            codeWord = BinaryStdIn.readInt(W);
+            // Stops or Breaks when it reaches the End of File
+            if (codeWord == EOF) {
+                break;
+            }
+            String entry;
+            // If the codewords exists in the dictionary, get its associated string
+            if (table[codeWord] != null) {
+                entry = table[codeWord];
+            }
+            // Edge case for LZW where the codeword is the next one to be defined
+            else {
+                entry = val + val.charAt(0);
+            }
+            // Add new dictionary entry if there is space
+            if (nextCode < L) {
+                table[nextCode++] = val + entry.charAt(0);
+            }
+            // Moves to the next decoded string
+            val = entry;
+        }
         BinaryStdOut.close();
     }
 
     public static void main(String[] args) {
-        if      (args[0].equals("-")) compress();
+        if (args[0].equals("-")) compress();
         else if (args[0].equals("+")) expand();
         else throw new IllegalArgumentException("Illegal command line argument");
     }
